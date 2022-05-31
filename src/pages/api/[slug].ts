941360f93be7be2ccdd445f478acd940224938a9
achintyajha <achintyakjha@gmail.com>
2022-05-31 13:27:04 +0530

    basic manual link shortening comp

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../db/client";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const slug = req.query["slug"];
+
+  if (!slug || typeof slug != "string") {
+    res.statusCode = 404;
+
+    res.send(JSON.stringify({ message: "Please use with a slug" }));
+
+    return;
+  }
+
+  const data = await prisma.shortLink.findFirst({
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
+
+  if (!data) {
+    res.statusCode = 404;
+
+    res.send(JSON.stringify({ message: "Slug not found" }));
+
+    return;
+  }
+
+  return res.redirect(data.url);
+};
